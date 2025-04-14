9e5a086abfa4d18d842abafa7a49770791352280
helenalenskjold <lenskjold2000@gmail.com>
2025-04-14 16:06:48 +0200

    opdateret js. manglede debugging

--- scripthl.js
+++ scripthl.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
   let interval = null;
+  console.log("DOM er klar.");
 
   $('.submit-btn').on('click', function () {
+    console.log("Submit-knap blev klikket.");
+
     const messages = [
       "Tusind tak for din støtte!",
       "Ses vi næste fredag?",
@@ -19,6 +22,8 @@ $(document).ready(function () {
       paid: "Mobilepay"
     };
 
+    console.log("Donationsdata:", donation);
+
     $('#type').text(donation.type);
     $('#amount').text(donation.amount + " DKK");
     $('#person').text(donation.person);
@@ -27,17 +32,21 @@ $(document).ready(function () {
     $('#paid').text(donation.paid);
 
     $('#confirmationModal').removeClass('hidden');
+    console.log("Bekræftelsesmodal vist.");
 
     if (interval !== null) {
       clearInterval(interval);
+      console.log("Tidligere interval ryddet.");
     }
 
     let i = 0;
     $('#confirmationText').text(messages[i]);
+    console.log("Første besked vist:", messages[i]);
     i++;
 
     interval = setInterval(function () {
       $('#confirmationText').text(messages[i]);
+      console.log("Ny besked vist:", messages[i]);
       i++;
       if (i >= messages.length) i = 0;
     }, 5000);
@@ -45,9 +54,11 @@ $(document).ready(function () {
 
   $('#closeBtn').on('click', function () {
     $('#confirmationModal').addClass('hidden');
+    console.log("Bekræftelsesmodal skjult.");
     if (interval !== null) {
       clearInterval(interval);
       interval = null;
+      console.log("Interval ryddet ved lukning.");
     }
   });
 
@@ -57,5 +68,6 @@ $(document).ready(function () {
     link.setSelectionRange(0, 99999);
     document.execCommand("copy");
     alert("Link kopieret!");
+    console.log("Link kopieret:", link.value);
   });
 });
